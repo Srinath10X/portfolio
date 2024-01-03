3a70a7e95596b7046cca1716b36158519181e622
Srinath10X <srinathcreates@gmail.com>
2024-01-04 02:03:26 +0530

    Added images and basic homepage

--- Components/Main.jsx
+++ Components/Main.jsx
@@ -1,9 +1,36 @@
 import React from "react";
+import Image from "next/image";
+import "./Main.css";
 
 const Main = () => {
   return (
     <>
-      <div>hello</div>
+      <div className="main">
+        <div className="left">
+          <h2 className="intro">Hello,I'm</h2>
+          <br />
+          <h1 className="name">Srinath</h1>
+          <p className="desc">
+            Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint
+          </p>
+          <div className="buttons">
+            <button className="btn">Contact</button>
+            <button className="btn">Download CV</button>
+          </div>
+        </div>
+        <div className="right">
+          <div className="wrap-img">
+            {/*<Image
+              className="img"
+              src="/anime-coding-1.jpeg"
+              height={400}
+              width={400}
+              alt=""
+            />*/}
+            <h1 className="x">10X</h1>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
