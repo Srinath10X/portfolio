50f917a3677e432f14a74cf4f4cf81e79eb88459
Srinath10X <srinathcreates@gmail.com>
2024-01-07 00:09:55 +0530

    Added about section layout

--- Components/About.jsx
+++ Components/About.jsx
@@ -5,9 +5,16 @@ const About = () => {
   return (
     <>
       <div className="about">
-        <div className="ab-right"></div>
+        <div className="ab-right">
+          <div className="img"></div>
+        </div>
         <div className="ab-left">
           <h2>About me</h2>
+          <hr className="line" />
+          <p className="about-desc">
+            Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint
+            cillum sint consectetur cupidatat.
+          </p>
         </div>
       </div>
     </>
