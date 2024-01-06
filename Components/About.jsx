4dc9b7cdb482870a9602676036050fde4a19ebf9
Srinath10X <srinathcreates@gmail.com>
2024-01-06 23:12:09 +0530

    Added About page

--- Components/About.jsx
+++ Components/About.jsx
@@ -1,9 +1,15 @@
 import React from "react";
+import "./About.css";
 
 const About = () => {
   return (
     <>
-      <div className="about"></div>
+      <div className="about">
+        <div className="ab-right"></div>
+        <div className="ab-left">
+          <h2>About me</h2>
+        </div>
+      </div>
     </>
   );
 };
