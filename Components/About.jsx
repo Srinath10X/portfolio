5d7b41c862dc564fa8e48f36b5f7a79a1c766dd9
Srinath10X <srinathcreates@gmail.com>
2024-01-07 11:22:40 +0530

    Added About page

--- Components/About.jsx
+++ Components/About.jsx
@@ -1,19 +1,24 @@
 import React from "react";
 import "./About.css";
+import Image from "next/image";
 
 const About = () => {
   return (
     <>
       <div className="about">
         <div className="ab-right">
-          <div className="img"></div>
+          <div className="img">
+            <Image src="/web.jpg" class="img-coding" height={300} width={300} />
+          </div>
         </div>
         <div className="ab-left">
           <h2>About me</h2>
           <hr className="line" />
           <p className="about-desc">
-            Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint
-            cillum sint consectetur cupidatat.
+            HTML, CSS, JavaScript enthusiast creating sleek and responsive
+            websites. React aficionado for interactive web experiences. Team
+            player 🤝 with a passion for user-centric design. Always learning
+            and adapting to the latest trends.
           </p>
         </div>
       </div>
