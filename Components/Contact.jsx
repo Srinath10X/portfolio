b449b73b40d19d780f8955bca8d30ae30572f51b
Srinath10X <srinathcreates@gmail.com>
2024-03-06 22:40:17 +0530

    Added contact.js

---
+++ Components/Contact.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Contact = () => {
+  return (
+    <>
+      <div className="contact">
+        <form action="" className="form">
+          <div className="title">Contact now</div>
+        </form>
+      </div>
+    </>
+  );
+};
+
+export default Contact;
