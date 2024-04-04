b8d14a84f2bbc7214431266dc03b97e47c6b8cbb
Srinath10X <srinathcreates@gmail.com>
2024-04-04 22:54:01 +0530

    configured the header to sticky header

--- app/page.jsx
+++ app/page.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import Header from "@/Components/Header";
 import Main from "@/Components/Main";
 import About from "@/Components/About";
-import Work from "@/Components/Work";
 import "./globals.css";
 
 const page = () => {
@@ -10,7 +9,6 @@ const page = () => {
     <>
       <Header />
       <Main />
-      <Work />
       <About />
     </>
   );
