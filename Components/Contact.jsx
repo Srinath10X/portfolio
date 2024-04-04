941662e77a2bd584ff35cd1d502d542ed3dd4a9a
Srinath10X <srinathcreates@gmail.com>
2024-04-05 00:06:12 +0530

    added contact form

--- Components/Contact.jsx
+++ Components/Contact.jsx
@@ -1,12 +1,20 @@
-import React from "react";
+"use client";
+import "./Contact.css";
 
 const Contact = () => {
   return (
     <>
-      <div className="contact">
-        <form action="" className="form">
-          <div className="title">Contact now</div>
+      <div class="contactForm">
+        <form action="https://formspree.io/f/xleqpqkb" method="POST">
+          <h1>Get In Touch</h1>
+          <input type="text" name="name" placeholder="Name" />
+          <input type="email" name="email" placeholder="Email" />
+          <textarea name="message" placeholder="Message"></textarea>
+          <button id="submit" type="submit">
+            Send
+          </button>
         </form>
+        <div class="right"></div>
       </div>
     </>
   );
--- app/page.jsx
+++ app/page.jsx
@@ -3,13 +3,14 @@ import Header from "@/Components/Header";
 import Main from "@/Components/Main";
 import About from "@/Components/About";
 import "./globals.css";
+import Contact from "@/Components/Contact";
 
 const page = () => {
   return (
     <>
       <Header />
       <Main />
-      <About />
+      <Contact />
     </>
   );
 };
