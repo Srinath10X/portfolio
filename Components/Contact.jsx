074accc7a77286f33dc60b207e4dd0afe25d9c42
Srinath10X <srinathcreates@gmail.com>
2024-04-19 00:56:11 +0530

    fix:requried name and email to contact

--- Components/Contact.jsx
+++ Components/Contact.jsx
@@ -8,9 +8,9 @@ const Contact = () => {
       <div class="contactForm" id="contact">
         <form action="https://formspree.io/f/xleqpqkb" method="POST">
           <h1 id="contact_title">Get In Touch</h1>
-          <input type="text" name="name" placeholder="Name" />
-          <input type="email" name="email" placeholder="Email" />
-          <textarea name="message" placeholder="Message"></textarea>
+          <input type="text" name="name" placeholder="Name" required />
+          <input type="email" name="email" placeholder="Email" required />
+          <textarea name="message" placeholder="Message" required></textarea>
           <button id="submit" type="submit">
             Send
           </button>
--- app/layout.jsx
+++ app/layout.jsx
@@ -1,12 +1,12 @@
 export const metadata = {
-  title: "Srinath10X - Portfolio",
-  description: "",
+	title: "Srinath10X - Portfolio",
+	description: "Welcome to my portfolio website",
 };
 
 export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
-  );
+	return (
+		<html lang="en">
+			<body>{children}</body>
+		</html>
+	);
 }
