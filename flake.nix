f00724dd14c849e868faef00ab53599d9020edc0
Srinath10X <srinathcreates@gmail.com>
2024-11-10 22:56:44 +0530

    chore: defined the devShell with Node.js v20.18.0 and npm v10.8.2

--- flake.nix
+++ flake.nix
@@ -1,15 +1,15 @@
 {
-  description = "A very basic flake";
+  description = "Srinath10X Portfolio 🚀";
 
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-  };
+  inputs.nixpkgs.url = "github:nixos/nixpkgs";
 
-  outputs = { self, nixpkgs }: {
-
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
-
-  };
+  outputs = { self, nixpkgs }:
+    let
+      pkgs = import nixpkgs { system = "x86_64-linux"; };
+    in
+    {
+      devShells.x86_64-linux.default = pkgs.mkShell {
+        buildInputs = [ pkgs.nodejs_20 ];
+      };
+    };
 }
