a57aff4e23c864a51fb4fffba00980845e6c1f2d
Srinath10X <srinathcreates@gmail.com>
2025-01-05 23:02:07 +0530

    chore: implemented blog logic

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,8 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import mdx from "@astrojs/mdx";
 
 export default defineConfig({
   integrations: [tailwind(), mdx()],
-  experimental: {
-    svg: true,
-  },
+  experimental: { svg: true },
 });
--- src/content.config.ts
+++ src/content.config.ts
@@ -1,7 +1,10 @@
+import { glob } from "astro/loaders";
 import { defineCollection, z } from "astro:content";
 
 export const collections = {
-  posts: defineCollection({
+  blog: defineCollection({
+    loader: glob({ base: "./src/content/blog", pattern: "**/*.{md,mdx}" }),
+
     schema: z.object({
       title: z.string(),
       description: z.string(),
