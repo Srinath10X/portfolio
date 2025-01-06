7fb7a67bc4bf4dc50c7acf30dfe58e2bd547b350
Srinath10X <srinathcreates@gmail.com>
2025-01-06 23:14:17 +0530

    refactor: "@" import aliases instead of ../.

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 
 export default defineConfig({
+  vite: { resolve: { alias: { "@": "/src" } } },
   integrations: [tailwind(), mdx()],
   experimental: { svg: true },
 });
